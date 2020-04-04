08e303d61f8cbd835d570fdb2c87e22b75c046a1
Reema <it.reema7@gmail.com>
2020-04-04 19:58:41 +0300

    Add files via upload

---
+++ Project 9/Drawing/Drawing/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Drawing
+//
+//  Created by Reema on 11/08/1441 AH.
+//  Copyright © 1441 Reema. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello world")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Project 9/Example1.swift
@@ -0,0 +1,29 @@
+//
+//  ContentView.swift
+//  Drawing
+//
+//  Created by Reema on 11/08/1441 AH.
+//  Copyright © 1441 Reema. All rights reserved.
+//  Creating custom paths with SwiftUI
+
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Path { path in
+            path.move(to: CGPoint(x: 200, y: 100))
+            path.addLine(to: CGPoint(x: 100, y: 300))
+            path.addLine(to: CGPoint(x: 300, y: 300))
+            path.addLine(to: CGPoint(x: 200, y: 100))
+            path.addLine(to: CGPoint(x: 100, y: 300))
+        }
+        .stroke(Color.blue, style: StrokeStyle(lineWidth: 10, lineCap: .round, lineJoin: .round))
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
