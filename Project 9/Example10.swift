98392776db15f2529ec49409f5b5e02d26ea9a5c
Reema <it.reema7@gmail.com>
2020-04-06 19:07:22 +0300

    Project 9 ✅

---
+++ Project 9/Example10.swift
@@ -0,0 +1,70 @@
+//
+//  ContentView.swift
+//  Drawing
+//
+//  Created by Reema on 11/08/1441 AH.
+//  Copyright © 1441 Reema. All rights reserved.
+//  Animating complex shapes with AnimatablePair
+
+import SwiftUI
+
+struct Checkerboard: Shape {
+    var rows: Int
+    var columns: Int
+    
+    public var animatableData: AnimatablePair<Double, Double> {
+        get {
+           AnimatablePair(Double(rows), Double(columns))
+        }
+
+        set {
+            self.rows = Int(newValue.first)
+            self.columns = Int(newValue.second)
+        }
+    }
+
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+
+        // figure out how big each row/column needs to be
+        let rowSize = rect.height / CGFloat(rows)
+        let columnSize = rect.width / CGFloat(columns)
+
+        // loop over all rows and columns, making alternating squares colored
+        for row in 0..<rows {
+            for column in 0..<columns {
+                if (row + column).isMultiple(of: 2) {
+                    // this square should be colored; add a rectangle here
+                    let startX = columnSize * CGFloat(column)
+                    let startY = rowSize * CGFloat(row)
+
+                    let rect = CGRect(x: startX, y: startY, width: columnSize, height: rowSize)
+                    path.addRect(rect)
+                }
+            }
+        }
+
+        return path
+    }
+}
+
+struct ContentView: View {
+    @State private var rows = 4
+    @State private var columns = 4
+
+    var body: some View {
+        Checkerboard(rows: rows, columns: columns)
+            .onTapGesture {
+                withAnimation(.linear(duration: 3)) {
+                    self.rows = 8
+                    self.columns = 16
+                }
+            }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
