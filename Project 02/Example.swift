4b273b9442acabffb7a2df4c608bdf0098fa62b3
Reema <it.reema7@gmail.com>
2020-04-11 19:27:06 +0300

    Project 2 ✅

---
+++ Project 02/Example.swift
@@ -0,0 +1,22 @@
+
+//
+//  Created by Reema on 18/08/1441 AH.
+//  Copyright © 1441 Reema. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        ZStack {
+            Text("Your content")
+                .background(Color.red)
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
