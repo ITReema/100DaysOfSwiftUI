c8d17664ce1ef67d5c0462a9801a2937a5fc178b
Reema <it.reema7@gmail.com>
2020-04-11 18:23:46 +0300

    Project 10 ✅

--- Project 10/Cupcake Corner/Cupcake Corner/ContentView.swift
+++ Project 10/Cupcake Corner/Cupcake Corner/ContentView.swift
@@ -4,14 +4,52 @@
 //
 //  Created by Reema on 14/08/1441 AH.
 //  Copyright © 1441 Reema. All rights reserved.
-//  
+//
 
 import SwiftUI
 
 struct ContentView: View {
-    
+    @ObservedObject var order = Order()
+
     var body: some View {
-        Text("Hello World")
+        NavigationView {
+            Form {
+                Section {
+                    Picker("Select your cake type", selection: $order.type) {
+                        ForEach(0..<Order.types.count, id: \.self) {
+                            Text(Order.types[$0])
+                        }
+                    }
+
+                    Stepper(value: $order.quantity, in: 3...20) {
+                        Text("Number of cakes: \(order.quantity)")
+                    }
+                }
+
+                Section {
+                    Toggle(isOn: $order.specialRequestEnabled.animation()) {
+                        Text("Any special requests?")
+                    }
+
+                    if order.specialRequestEnabled {
+                        Toggle(isOn: $order.extraFrosting) {
+                            Text("Add extra frosting")
+                        }
+
+                        Toggle(isOn: $order.addSprinkles) {
+                            Text("Add extra sprinkles")
+                        }
+                    }
+                }
+
+                Section {
+                    NavigationLink(destination: AddressView(order: order)) {
+                        Text("Delivery details")
+                    }
+                }
+            }
+            .navigationBarTitle("Cupcake Corner")
+        }
     }
 }
 
