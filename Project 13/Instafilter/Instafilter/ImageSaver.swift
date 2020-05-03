212277cd5db5451348a3a42110b239401718a6cf
Reema <it.reema7@gmail.com>
2020-05-03 23:39:32 +0300

    Project 13 ✅

---
+++ Project 13/Instafilter/Instafilter/ImageSaver.swift
@@ -0,0 +1,27 @@
+//
+//  ImageSaver.swift
+//  Instafilter
+//
+//  Created by Reema on 10/09/1441 AH.
+//  Copyright © 1441 Reema. All rights reserved.
+//
+
+import UIKit
+
+class ImageSaver: NSObject {
+    var successHandler: (() -> Void)?
+    var errorHandler: ((Error) -> Void)?
+    
+    func writeToPhotoAlbum(image: UIImage) {
+        UIImageWriteToSavedPhotosAlbum(image, self, #selector(saveError), nil)
+    }
+
+    @objc func saveError(_ image: UIImage, didFinishSavingWithError error: Error?, contextInfo: UnsafeRawPointer) {
+        // save complete
+        if let error = error {
+            errorHandler?(error)
+        } else {
+            successHandler?()
+        }
+    }
+}
