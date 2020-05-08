ab34fe5ccbd934f0d1768006b20027ed975f4626
Reema <it.reema7@gmail.com>
2020-05-08 04:49:25 +0300

    Add files via upload

--- Project 14/Example4/BucketList/ContentView.swift
+++ Project 14/Example4/BucketList/ContentView.swift
@@ -10,8 +10,8 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
-    }
+        MapView()
+            .edgesIgnoringSafeArea(.all)    }
 }
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
---
+++ Project 14/Example5/BucketList/ContentView.swift
@@ -0,0 +1,56 @@
+//
+//  ContentView.swift
+//  BucketList
+//
+//  Created by Reema on 11/09/1441 AH.
+//  Copyright © 1441 Reema. All rights reserved.
+//
+
+import LocalAuthentication
+import SwiftUI
+
+struct ContentView: View {
+    @State private var isUnlocked = false
+    
+    var body: some View {
+        VStack {
+            if self.isUnlocked {
+                Text("Unlocked")
+            } else {
+                Text("Locked")
+            }
+        }
+        .onAppear(perform: authenticate)
+    }
+    
+    func authenticate() {
+        let context = LAContext()
+        var error: NSError?
+
+        // check whether biometric authentication is possible
+        if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) {
+            // it's possible, so go ahead and use it
+            let reason = "We need to unlock your data."
+
+            context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: reason) { success, authenticationError in
+                // authentication has now completed
+                DispatchQueue.main.async {
+                    if success {
+                        // authenticated successfully
+                        self.isUnlocked = true
+                    } else {
+                        // there was a problem
+                    }
+                }
+            }
+        } else {
+            // no biometrics
+        }
+    }
+        
+}
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
