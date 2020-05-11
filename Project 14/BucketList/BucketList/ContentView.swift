f47a7d7814c141ebacd325c4aab06eb668212000
Reema <it.reema7@gmail.com>
2020-05-12 00:26:10 +0300

    Add files via upload

--- Project 14/BucketList/BucketList/ContentView.swift
+++ Project 14/BucketList/BucketList/ContentView.swift
@@ -6,11 +6,51 @@
 //  Copyright © 1441 Reema. All rights reserved.
 //
 
+import MapKit
 import SwiftUI
 
 struct ContentView: View {
+    @State private var centerCoordinate = CLLocationCoordinate2D()
+    @State private var locations = [MKPointAnnotation]()
+    @State private var selectedPlace: MKPointAnnotation?
+    @State private var showingPlaceDetails = false
+    
     var body: some View {
-        Text("Hello, World!")
+        ZStack {
+            MapView(centerCoordinate: $centerCoordinate, selectedPlace: $selectedPlace, showingPlaceDetails: $showingPlaceDetails, annotations: locations)
+                .edgesIgnoringSafeArea(.all)
+            Circle()
+                .fill(Color.blue)
+                .opacity(0.3)
+                .frame(width: 32, height: 32)
+            
+            VStack {
+                Spacer()
+                HStack {
+                    Spacer()
+                    Button(action: {
+                        // create a new location
+                        let newLocation = MKPointAnnotation()
+                        newLocation.title = "Example location"
+                        newLocation.coordinate = self.centerCoordinate
+                        self.locations.append(newLocation)
+                    }) {
+                        Image(systemName: "plus")
+                    }
+                    .padding()
+                    .background(Color.black.opacity(0.75))
+                    .foregroundColor(.white)
+                    .font(.title)
+                    .clipShape(Circle())
+                    .padding(.trailing)
+                }
+            }
+        }
+        .alert(isPresented: $showingPlaceDetails) {
+            Alert(title: Text(selectedPlace?.title ?? "Unknown"), message: Text(selectedPlace?.subtitle ?? "Missing place information."), primaryButton: .default(Text("OK")), secondaryButton: .default(Text("Edit")) {
+                // edit this place
+            })
+        }
     }
 }
 struct ContentView_Previews: PreviewProvider {
