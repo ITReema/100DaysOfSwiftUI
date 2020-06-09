49ae9e4b42895967c85e9e73f5b0fd1768e7ebb9
Reema <it.reema7@gmail.com>
2020-06-09 08:13:16 +0300

    Add files via upload

--- Project 17/Flashzilla/Flashzilla/CardView.swift
+++ Project 17/Flashzilla/Flashzilla/CardView.swift
@@ -10,7 +10,9 @@ import SwiftUI
 
 struct CardView: View {
     let card: Card
+    var removal: (() -> Void)? = nil
     @State private var isShowingAnswer = false
+    @State private var offset = CGSize.zero
     
     var body: some View {
         ZStack {
@@ -33,6 +35,24 @@ struct CardView: View {
             .multilineTextAlignment(.center)
         }
         .frame(width: 450, height: 250)
+        .rotationEffect(.degrees(Double(offset.width / 5)))
+        .offset(x: offset.width * 5, y: 0)
+        .opacity(2 - Double(abs(offset.width / 50)))
+        .gesture(
+            DragGesture()
+                .onChanged { gesture in
+                    self.offset = gesture.translation
+                }
+
+                .onEnded { _ in
+                    if abs(self.offset.width) > 100 {
+                        // remove the card
+                        self.removal?()
+                    } else {
+                        self.offset = .zero
+                    }
+                }
+        )
         .onTapGesture {
             self.isShowingAnswer.toggle()
         }
