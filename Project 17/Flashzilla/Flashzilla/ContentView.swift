393d30754355d53e08e845deb2094a37c9a3e312
Reema <it.reema7@gmail.com>
2020-06-10 21:13:02 +0300

    Add files via upload

--- Project 17/Flashzilla/Flashzilla/ContentView.swift
+++ Project 17/Flashzilla/Flashzilla/ContentView.swift
@@ -16,6 +16,8 @@ extension View {
 }
 
 struct ContentView: View {
+    @Environment(\.accessibilityDifferentiateWithoutColor) var differentiateWithoutColor
+
     @State private var cards = [Card](repeating: Card.example, count: 10)
     
     var body: some View {
@@ -34,6 +36,26 @@ struct ContentView: View {
                         }
                         .stacked(at: index, in: self.cards.count)
                     }
+                }
+            }
+            if differentiateWithoutColor {
+                VStack {
+                    Spacer()
+
+                    HStack {
+                        Image(systemName: "xmark.circle")
+                            .padding()
+                            .background(Color.black.opacity(0.7))
+                            .clipShape(Circle())
+                        Spacer()
+                        Image(systemName: "checkmark.circle")
+                            .padding()
+                            .background(Color.black.opacity(0.7))
+                            .clipShape(Circle())
+                    }
+                    .foregroundColor(.white)
+                    .font(.largeTitle)
+                    .padding()
                 }
             }
         }
