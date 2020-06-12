2db444f9a3afda6288b52aa17a1ce0f2510833c3
Reema <it.reema7@gmail.com>
2020-06-12 20:46:08 +0300

    Add files via upload

--- Project 17/Flashzilla/Flashzilla/CardView.swift
+++ Project 17/Flashzilla/Flashzilla/CardView.swift
@@ -14,6 +14,7 @@ struct CardView: View {
     var removal: (() -> Void)? = nil
     @State private var isShowingAnswer = false
     @State private var offset = CGSize.zero
+    @State private var feedback = UINotificationFeedbackGenerator()
     
     var body: some View {
         ZStack {
@@ -53,13 +54,20 @@ struct CardView: View {
         .opacity(2 - Double(abs(offset.width / 50)))
         .gesture(
             DragGesture()
-                .onChanged { gesture in
-                    self.offset = gesture.translation
+               .onChanged { offset in
+                    self.offset = offset.translation
+                    self.feedback.prepare()
                 }
 
                 .onEnded { _ in
                     if abs(self.offset.width) > 100 {
                         // remove the card
+                        if self.offset.width > 0 {
+                            self.feedback.notificationOccurred(.success)
+                        } else {
+                            self.feedback.notificationOccurred(.error)
+                        }
+
                         self.removal?()
                     } else {
                         self.offset = .zero
